e22e7597ada4d94764040c961097ca4c820cf121
Jim O'Brien <jim@jimmed.net>
2018-12-07 01:15:14 +0000

    Add basic progress tracking to tasks

--- src/runTask.js
+++ src/runTask.js
@@ -3,16 +3,31 @@ import { type TaskDefinition } from './types.flow';
 import findTask from './findTask';
 import undoTask from './undoTask';
 
+type ChildTaskDefinition = {
+  task: TaskDefinition,
+  args: *,
+  promise: Promise<*>,
+};
+
+type ProgressState = {
+  total: number,
+  done: number,
+};
+
 export default async function runTask(
   tasks: Array<TaskDefinition>,
   task: TaskDefinition,
   args: *,
-): $Call<$PropertyType<TaskDefinition, 'run'>> => {
-  const childTasks = [];
+): $Call<$PropertyType<TaskDefinition, 'run'>> {
+  const childTasks: Array<ChildTaskDefinition> = [];
+  let progress: ProgressState;
 
   // Declare methods we will pass to our task runner method
   const actions = {
-    childTask(childTaskName, childArgs) {
+    childTask(
+      childTaskName: $PropertyType<TaskDefinition, 'name'>,
+      childArgs: *,
+    ) {
       const matched = findTask(tasks, childTaskName);
       const childTask = {
         task: matched,
@@ -20,6 +35,26 @@ export default async function runTask(
         promise: runTask(tasks, matched, childArgs),
       };
       childTasks.push(childTask);
+    },
+    getState(): ProgressState {
+      return progress || { total: 0, done: 0 };
+    },
+    setState(change: $Shape<ProgressState>) {
+      progress = { ...this.getState(), ...change };
+    },
+    setTotal(total: number) {
+      this.setState({ total });
+    },
+    incTotal(delta: number = 1) {
+      const { total } = this.getState();
+      this.setTotal(total + delta);
+    },
+    setComplete(complete: number) {
+      this.setState({ complete });
+    },
+    incComplete(delta: number = 1) {
+      const { complete } = this.getState();
+      this.setComplete(complete + delta);
     },
   };
 
